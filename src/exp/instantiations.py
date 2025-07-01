75c5b31e51f4452b8011d0293d43263ebce3f38f
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-07-01 02:56:15 +0000

    feat(config): refactor stacked hidden models and trainers configuration

--- src/exp/instantiations.py
+++ src/exp/instantiations.py
@@ -22,12 +22,9 @@ def instantiate_interaction(cfg: DictConfig) -> Interaction[Any, Any]:
 
 def instantiate_models(cfg: DictConfig) -> dict[ModelName, TorchTrainingModel[Any]]:
     logger.info("Instantiating Models...")
-    model_cfg = cfg.models
     device, dtype = cfg.shared.device, cfg.shared.dtype
 
-    from exp.envs.vrchat import OSC_ACTION_CHOICES
     from exp.models.jepa import create_image_jepa
-    from exp.models.utils import ActionInfo, ObsInfo
 
     logger.info("Instantiating JEPA models...")
     context_encoder, target_encoder, predictor, avg_pool_infer = create_image_jepa(
@@ -41,31 +38,7 @@ def instantiate_models(cfg: DictConfig) -> dict[ModelName, TorchTrainingModel[An
         output_downsample=3,
     )
 
-    # Setup config
-    obs_info = ObsInfo(
-        dim=target_encoder.embed_dim,
-        dim_hidden=1024,
-        num_tokens=avg_pool_infer.output_patch_count,
-    )
-    action_info = ActionInfo(list(OSC_ACTION_CHOICES), dim=8)
-    with open_dict(model_cfg) as opened_cfg:
-        piv_cfg = opened_cfg[ModelName.POLICY_VALUE]
-        piv_cfg.obs_info = obs_info
-        piv_cfg.action_choices = OSC_ACTION_CHOICES
-
-        fd_cfg = opened_cfg[ModelName.FORWARD_DYNAMICS]
-        fd_cfg.obs_info = obs_info
-        fd_cfg.action_info = action_info
-
-    # Instantiate Policy Value model.
-    logger.info("Instantiating Policy...")
-    policy_value = hydra.utils.instantiate(piv_cfg)
-
-    # Instantiate Forward Dynamics model.
-    logger.info("Instantiating Forward Dynamics...")
-    forward_dynamics = hydra.utils.instantiate(fd_cfg)
-
-    return {
+    models = {
         ModelName.IMAGE_JEPA_CONTEXT_ENCODER: TorchTrainingModel(
             context_encoder,
             has_inference_model=False,
@@ -85,16 +58,15 @@ def instantiate_models(cfg: DictConfig) -> dict[ModelName, TorchTrainingModel[An
             device=device,
             dtype=dtype,
         ),
-        ModelName.FORWARD_DYNAMICS: TorchTrainingModel(
-            forward_dynamics,
-            has_inference_model=True,
-            device=device,
-            dtype=dtype,
-        ),
-        ModelName.POLICY_VALUE: TorchTrainingModel(
-            policy_value, has_inference_model=True, device=device, dtype=dtype
-        ),
     }
+
+    # Instantiating
+    for name, model_cfg in cfg.models.items():
+        logger.info(f"Instantiating model: '{name}' ...")
+
+        models[ModelName(name)] = hydra.utils.instantiate(model_cfg)
+
+    return models
 
 
 def instantiate_trainers(cfg: DictConfig) -> dict[str, TorchTrainer]:
@@ -126,12 +98,9 @@ def instantiate_trainers(cfg: DictConfig) -> dict[str, TorchTrainer]:
         min_new_data_count=128,
     )
 
-    trainer_cfg = cfg.trainers
+    trainers_dict: dict[str, TorchTrainer] = {"jepa": jepa}
+    for name, trainer_cfg in cfg.trainers.items():
+        logger.info(f"Instantiating Trainer: '{name}' ...")
+        trainers_dict[name] = hydra.utils.instantiate(trainer_cfg)
 
-    logger.info("Instantiating Forward Dynamics Trainer...")
-    forward_dynamics = hydra.utils.instantiate(trainer_cfg.forward_dynamics)
-
-    logger.info("Instantiating Policy Trainer...")
-    policy_value = hydra.utils.instantiate(trainer_cfg.policy)
-
-    return {"jepa": jepa, "forward_dynamics": forward_dynamics, "policy": policy_value}
+    return trainers_dict
