463340cf7b2f018283c25e2912432b17a89a047a
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-07-17 15:24:10 +0900

    refactor(models): remove latent_fd module

--- src/exp/models/names.py
+++ src/exp/models/names.py
@@ -11,6 +11,3 @@ class ModelName(StrEnum):
     FORWARD_DYNAMICS = "forward_dynamics"
 
     POLICY_VALUE = "policy_value"
-
-    LATENT_FD_ENCODER = "latent_fd_encoder"
-    LATENT_FD_PREDICTOR = "latent_fd_predictor"
