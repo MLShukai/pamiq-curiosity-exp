4ee4a26f264a63ef4471fe93f410a50b394db78a
myxyy <m.myxomatosis@gmail.com>
2026-02-14 20:35:57 +0900

    maxpool

--- src/exp/models/components/cnn_encoder.py
+++ src/exp/models/components/cnn_encoder.py
@@ -20,23 +20,32 @@ class CNNEncoder(nn.Module):
         super().__init__()
         conv_output_height = input_height
         conv_output_width = input_width
-        self.conv1 = nn.Conv2d(input_channels, 32, kernel_size=3, stride=2, padding=0)
-        conv_output_height = (conv_output_height - (3 - 1) - 1) // 2 + 1
-        conv_output_width = (conv_output_width - (3 - 1) - 1) // 2 + 1
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=0)
-        conv_output_height = (conv_output_height - (3 - 1) - 1) // 2 + 1
-        conv_output_width = (conv_output_width - (3 - 1) - 1) // 2 + 1
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=0)
-        conv_output_height = (conv_output_height - (3 - 1) - 1) // 2 + 1
-        conv_output_width = (conv_output_width - (3 - 1) - 1) // 2 + 1
+
+        self.norm_in = nn.InstanceNorm2d(3)
+
+        self.conv1 = nn.Conv2d(input_channels, 4, kernel_size=5, padding=2)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        conv_output_height = conv_output_height // 2
+        conv_output_width = conv_output_width // 2
+
+        self.conv2 = nn.Conv2d(4, 16, kernel_size=5, padding=2)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        conv_output_height = conv_output_height // 2
+        conv_output_width = conv_output_width // 2
+
+        self.conv3 = nn.Conv2d(16, 64, kernel_size=5, padding=2)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        conv_output_height = conv_output_height // 2
+        conv_output_width = conv_output_width // 2
+
         self.flatten = nn.Flatten()
 
         # Calculate the size of the feature map after convolutions
-        conv_output_size = 128 * conv_output_height * conv_output_width
+        conv_output_size = 64 * conv_output_height * conv_output_width
+        self.fc = nn.Linear(conv_output_size, output_dim)
+        self.norm_out = nn.LayerNorm(output_dim)
 
         self.act = nn.SiLU()
-
-        self.fc = nn.Linear(conv_output_size, output_dim)
 
     @override
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -51,11 +60,13 @@ class CNNEncoder(nn.Module):
         no_batch = x.ndim == 3
         if no_batch:
             x = x.unsqueeze(0)  # Add batch dimension if missing
-        x = self.act(self.conv1(x))
-        x = self.act(self.conv2(x))
-        x = self.act(self.conv3(x))
+        x = self.norm_in(x)
+        x = self.pool1(self.act(self.conv1(x)))
+        x = self.pool2(self.act(self.conv2(x)))
+        x = self.pool3(self.act(self.conv3(x)))
         x = self.flatten(x)
         x = self.fc(x)
+        x = self.norm_out(x)
         if no_batch:
             x = x.squeeze(0)
         return x
